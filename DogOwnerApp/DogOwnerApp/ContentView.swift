3f315e8851d97e3203f7fd43c2b429d51211e227
semajzandrews <semajzandrews@gmail.com>
2022-07-20 18:55:54 -0400

    Created Initial Project Files

---
+++ DogOwnerApp/DogOwnerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogOwnerApp
+//
+//  Created by Semaj Andrews on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogOwnerApp/DogOwnerApp/DogOwnerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogOwnerAppApp.swift
+//  DogOwnerApp
+//
+//  Created by Semaj Andrews on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DogOwnerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
