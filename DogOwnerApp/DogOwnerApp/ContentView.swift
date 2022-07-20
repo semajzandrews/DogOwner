1a637fb88171848d95ac97b12c31dad8f1e8aef8
semajzandrews <semajzandrews@gmail.com>
2022-07-20 19:21:36 -0400

    Implemented Login Flow Text Fields UI

--- DogOwnerApp/DogOwnerApp/ContentView.swift
+++ DogOwnerApp/DogOwnerApp/ContentView.swift
@@ -8,16 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
 
         ZStack {
             Color.black
             RoundedRectangle(cornerRadius: 30, style: .continuous)
-                .foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .foregroundStyle(.linearGradient(colors: [.blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing))
                 .frame(width: 1000, height: 400)
                 .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            VStack(spacing: 20){
+                Text("Welcome")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .serif))
+                    .offset(x: -100, y: -100)
+                
+                TextField("Email", text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty){
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty){
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+
+                                            }
+            }
+            .frame(width: 350)
         }
         .ignoresSafeArea()
+    }
+}
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
     }
 }
 
